9f35697f06589a89e014d7612372b1726bea0be2
q <69131802+earthmite@users.noreply.github.com>
2022-02-23 19:07:49 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const mainPath = path.resolve("./public/main/index.html");
+const encodePath = path.resolve("./public/encode/encode.html");
+const decodePath = path.resolve("./public/decode/decode.html");
+
+const app = express();
+const port = process.env.PORT || 4756;
+
+app.get("/", (req, res) => {
+  res.sendFile(mainPath);
+});
+
+
+app.get("/encode", (req, res) => {
+  res.sendFile(encodePath);
+});
+
+app.get("/decode", (req, res) => {
+  res.sendFile(decodePath);
+});
+
+app.use(express.static("main"));
+
+app.use(express.static("encode"));
+
+app.use(express.static("decode"));
+
+app.listen(port, () => {
+  console.log(`Running service at ${port}`);
+});
